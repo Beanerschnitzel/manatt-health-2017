f32c26ca02d6a0a4592bcfb68756374b48e1f12a
ElTigreBoyer <beanerschnitzel@gmail.com>
2017-03-27 14:34:26 -0600

    ui

--- js/front-end/mobileSubToggles.js
+++ js/front-end/mobileSubToggles.js
@@ -1,66 +1,33 @@
-/*******************************************************************
-This is the function to add toggles for nav items that have children.
-If this is going in a mobile Drawer the mediaQuery needs to match the 
- mediaQuery used in the drawer function.
-*******************************************************************/
+//toggle search on lg screens
 
-$(document).ready(function() {
-  $('.headerMain-navItems li.hasChildren').subToggle({
-    mediaQuery: 1024
-  });
-});
+jQuery(document).ready(function($) {
+    var mqlSubToggles = window.matchMedia("(max-width: 768px)");
 
-//jQuery function for adding sub toggles
-(function($) {
-  $.fn.subToggle = function(options) {
-    var opts = options;
-    return this.each(function() {
-      var defaults = {
-        mediaQuery: 1024
-      };
-      var options = $.extend({}, defaults, opts);
-
-      var $this = $(this);
-      var mediaQuery = options.mediaQuery;
-
-      addRemoveToggle($this, mediaQuery);
-
-      $(window).on('resize orientationchange', function() {
-        addRemoveToggle($this, mediaQuery);
-      });
-    });
-  }
-}(jQuery));
-
-//Function to add/remove toggles
-//This is called by the plugin above this.
-function addRemoveToggle(element, mediaQuery) {
-  if (!window.matchMedia('(min-width: ' + mediaQuery + 'px)').matches) {
-    if (element.find('.navMain-mobileSubToggle').length <= 0) {
-      element.prepend('<span class="navMain-mobileSubToggle" />');
+    function addToggles() {
+        $('.headerMain-navItems li.hasChildren').prepend('<span class="navMain-mobileSubToggle" />'); 
     }
-  } else {
-    element.find('.navMain-mobileSubToggle').remove();
-  }
-}
 
-//Document delegation for mobils sub toggles.
-//Delegating this event prevents us from having to wire this up each time the toggle is added. 
-$(document).on('click', '.navMain-mobileSubToggle', function(event) {
-  event.stopPropagation();
-  if (!$(this).closest('li').hasClass('is-open')) {
-    $('.headerMain-navItems li.is-open').removeClass('is-open');
-    $(this).closest('li').addClass('is-open');
-  } else {
-    $('.headerMain-navItems li.is-open').removeClass('is-open');
-  }
-});
+    function toggleSubs() {
+        $('.navMain-mobileSubToggle').on("click.mqlSubToggles", function() {
+            $(this).closest('li').toggleClass('is-open');
+        }); 
+    }
 
-//Document delegation for links with no destination (act as a sub toggle).
-$(document).on('click', '.headerMain-navItems li.hasChildren', function(event) {
-  if ($(this).find('a').attr('href') === "javascript:void(0)") {
-    event.preventDefault();
-    $('.headerMain-navItems li.hasChildren').not($(this)).removeClass('is-open');
-    $(this).toggleClass('is-open');
-  }
+    function resetToggles() {
+        $('.navMain-mobileSubToggle').remove();
+        $('.headerMain-navItems li.is-open').removeClass('is-open');
+    }
+
+    function screenSize(mqlSubToggles) {
+      if (mqlSubToggles.matches) {
+        addToggles();
+        toggleSubs();
+      } else {
+        resetToggles();
+      }
+    }
+
+    // Handle media query 'change' event
+    mqlSubToggles.addListener(screenSize);
+    screenSize(mqlSubToggles);
 });
